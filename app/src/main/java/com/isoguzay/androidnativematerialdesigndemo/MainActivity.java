77c337bdb0f787a0c78dc0cb4401a1e67b1d9b39
ismailoguzhanay <isoguzay@gmail.com>
2019-07-29 00:01:43 +0300

    Material Design Demo

---
+++ app/src/main/java/com/isoguzay/androidnativematerialdesigndemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.isoguzay.androidnativematerialdesigndemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
